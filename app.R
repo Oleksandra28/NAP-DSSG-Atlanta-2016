5b4488dec377bed3e74f821a98b4acdc7657e212
Oleksandra Sopova <oleksandrasopova@gmail.com>
2016-05-31 15:18:12 -0400

    reading csv data files added

--- app.R
+++ app.R
@@ -2,7 +2,12 @@
    library(leaflet)
    library(magrittr)
    library(rgdal)
-   
+
+   library(readxl) #IF NOT INSTALLED, NEED TO: install.packages("readxl")
+
+   Properties <- read.csv('Potential-Properties.csv')
+   TStops <- read.csv('TStops.csv')
+   Schools <- read.csv('Schools.csv')
   ## User Interface - Title, headings, and sidebar 
    ui <- fluidPage(
      titlePanel("New American Pathways Housing Scout"), br(), h2("Selection Criteria"),
@@ -31,9 +36,9 @@
           addTiles(urlTemplate = "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png") %>%  
           setView(-84.3959808,33.7769859, zoom = 10)%>%
           addCircles(lat = ~latitude, lng = ~longitude, data= Properties, weight = 5, radius = 80, fillOpacity = .05, popup = Properties$Name, group = "Units")%>%
-          addCircles(lat = ~Lat, lng = ~ Lon, data= TStops, color="#ffa500", weight = 1, radius = 10, fillOpacity = .03, group = "Transit")%>%
+          addCircles(lat = ~Lat, lng = ~Lon, data= TStops, color="#ffa500", weight = 1, radius = 10, fillOpacity = .03, group = "Transit")%>%
           addCircles(lat = ~ Y, lng = ~X, data = Schools, color="#e01d5d", weight = 4, radius = 100, popup = Schools$FACNAME, group ="Schools")%>%
-          addLayersControl(overlayGroups = c("Units", "Transit", "Schools"))%>%
+          addLayersControl(overlayGroups = c("Units", "Transit", "Schools")) #%>%
           ##addLegend(position = topleft, )
        
      })
